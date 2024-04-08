d9360728f1a6e940054cabe9e8dab514e9d644a5
kbrady478 <c23789389@mytudublin.ie>
2024-04-08 11:43:46 +0100

    Metronome Accent + Bus
    Accented the metronome, there are more complex ways to do this but I just had two different ASP's, may change this as I might add the more complex version to other areas
    Also added a dedicated Metronome audio bus, plan on having a slider to control it's volume.

--- Scripts/Metronome.gd
+++ Scripts/Metronome.gd
@@ -4,16 +4,20 @@ var bpm
 var current_Wait_Time
 var new_Wait_Time
 var metronome_Toggle
+var metronome_Accent
+var accent_Tracker
 
 @onready var metronome_Timer = $Metronome_Timer
 @onready var metronome_Sound = $Metronome_Sound
-
+@onready var metronome_Accented = $Metronome_Accented
 
 func _ready():
 	bpm = 180.0
 	current_Wait_Time = 0.5
 	new_Wait_Time = 0.1
 	metronome_Toggle = true
+	metronome_Accent = 4
+	accent_Tracker = 0
 	metronome()
 
 func bpm_Calculation():
@@ -30,9 +34,20 @@ func metronome():
 			bpm_Calculation()
 		
 		print_debug(current_Wait_Time)
-		metronome_Sound.play()
-		metronome_Timer.set_wait_time(current_Wait_Time)
-		metronome_Timer.start()
+		
+		if accent_Tracker == 0:
+			metronome_Accented.play()
+			metronome_Timer.set_wait_time(current_Wait_Time)
+			metronome_Timer.start()
+			accent_Tracker += 1
+		else:
+			metronome_Sound.play()
+			metronome_Timer.set_wait_time(current_Wait_Time)
+			metronome_Timer.start()
+			accent_Tracker += 1
+			if accent_Tracker == 4:
+				accent_Tracker = 0
+			
 
 func _on_metronome_timer_timeout():
 	metronome()
