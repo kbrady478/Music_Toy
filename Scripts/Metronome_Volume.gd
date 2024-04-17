2022376c70f73165fa1b824a34dcc9fe9ac25388
kbrady478 <c23789389@mytudublin.ie>
2024-04-17 13:20:20 +0100

    Restarted metronome volume controller
    Needed a blank slate, now volume slider and mute toggle interact within the same script, will create something similar for other audio sources not related to a bus

---
+++ Scripts/Metronome_Volume.gd
@@ -0,0 +1,31 @@
+extends Control
+
+@export var bus_Name: String
+
+var bus_Index: int
+var volume_Level : float # for use across functions
+var mute_Toggle: bool
+
+
+func _ready() -> void:
+	bus_Index = AudioServer.get_bus_index(bus_Name)
+	mute_Toggle = false
+	
+
+
+func _on_volume_slider_value_changed(value) -> void:
+	volume_Level = value
+	
+	if mute_Toggle == true:
+		AudioServer.set_bus_volume_db(bus_Index, linear_to_db(0))
+	else:
+		AudioServer.set_bus_volume_db(bus_Index, linear_to_db(volume_Level))
+
+
+func _on_mute_button_pressed():
+	mute_Toggle = not mute_Toggle
+	
+	if mute_Toggle == true:
+		AudioServer.set_bus_volume_db(bus_Index, linear_to_db(0))
+	else:
+		AudioServer.set_bus_volume_db(bus_Index, linear_to_db(volume_Level))
